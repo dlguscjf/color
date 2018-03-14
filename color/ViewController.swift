3fc8ba3bbf8d233783bd56f044364960437a3c04
D7703_06 <d7703_06@D7703-06ui-iMac.local>
2018-03-14 14:38:19 +0900

    Changecolor

--- color/ViewController.swift
+++ color/ViewController.swift
@@ -9,17 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var a = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.yellow
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func button(_ sender: UIButton) {
+        
+        if a == 1{
+            view.backgroundColor = UIColor.yellow
+            a += 1
+        }
+        else if a == 2
+        {
+            view.backgroundColor = UIColor.red
+            a = 1
+        }
+    }
+    
 }
 
