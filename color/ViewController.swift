85bdbfcdea16456159a4fb3e4fcf1cb1de67e5cc
D7703_06 <d7703_06@D7703-06ui-iMac.local>
2018-03-14 14:47:58 +0900

    add switch

--- color/ViewController.swift
+++ color/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     var a = 1
+    var a1 = 1
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -31,6 +32,20 @@ class ViewController: UIViewController {
         {
             view.backgroundColor = UIColor.red
             a = 1
+        }
+    }
+    @IBAction func button2(_ sender: UIButton) {
+        switch a {
+        case(1) :
+            view.backgroundColor = UIColor.blue
+            a += 1
+        case(2) :
+            view.backgroundColor = UIColor.red
+            a += 1
+        default:
+            view.backgroundColor = UIColor.yellow
+            a = 1
+      
         }
     }
     
